abb8c101fb0ecdb94cce593c14c7c025e19469d7
Sarah <70615195+smanter82@users.noreply.github.com>
2020-10-14 18:34:20 -0400

    set up bootstrap containers in html and added start quiz button

---
+++ index.js
@@ -0,0 +1,23 @@
+// let btn = document.querySelector(".startButton")
+// let question = document.querySelector("#quest")
+// let questions = []
+// {
+//     question1: "Commonly used data types do NOT include",
+//     choices:
+//     correct:
+// },
+// {
+//     question2:
+//     choices:
+//     correct:
+// }
+
+// function startQuiz() {
+//     var x = document.getElementById("quest");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//       console.log("Hey!")
+//     } else {
+//       x.style.display = "none";
+//     }
+// }
