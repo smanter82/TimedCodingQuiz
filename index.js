41ab0721467f00421023fd464cf5e2406e679fb9
Sarah <70615195+smanter82@users.noreply.github.com>
2020-10-14 20:42:54 -0400

    start function toggle interface

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 let btn = document.querySelector(".startButton")
-let question = document.querySelector("#quest")
+let quiz = document.querySelector(".quizCard")
+
 let questions = [
 //questions and answers taken from sample quiz in assignment's Read Me file
 {
     question1: "Commonly used data types do NOT include",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    correct:  "alerts"
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        correct:  "alerts"
 },
 {
     question2: "String values must be enclosed within _____ when being assigned to variables",
-    choices: ["commas", "curly brackets", "quotes", "parentheses"],
-    correct:  "parentheses"
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        correct:  "parentheses"
 },
 {
     question3: "The condition in an if/else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    correct:  "parentheses"
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        correct:  "parentheses"
 }]
 
 function startQuiz() {
-    var x = document.getElementById("quest");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      console.log("Hello!")
-    } else {
-      x.style.display = "none";
-    }
+    var x = document.querySelector(".beginQuiz");
+    x.style.display = "none"
+    quiz.style.display = "block"
+    //   console.log(questions[0].choices[1])
+    
 }
