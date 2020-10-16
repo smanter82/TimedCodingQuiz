3171f58da0e8d82fe81e0a132422a3fcdd0fe9a3
Sarah <70615195+smanter82@users.noreply.github.com>
2020-10-15 21:54:31 -0400

    fixed countdown timer to countdown on page from 60 to 0

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let btn = document.querySelector(".startButton")
 let quiz = document.querySelector(".quizCard")
+let timer = 60
+let counter = document.querySelector(".countDown")
 
 let questions = [
 //questions and answers taken from sample quiz in assignment's Read Me file
@@ -21,8 +23,26 @@ let questions = [
 
 function startQuiz() {
     var x = document.querySelector(".beginQuiz");
+    
     x.style.display = "none"
     quiz.style.display = "block"
     //   console.log(questions[0].choices[1])
+    countDown()  
+    //call new function (question/answer)  
+}
+
+function countDown () {
+    
+    const myTimer = setInterval(function() {
+        counter.textContent = timer + " seconds left!"
+        timer--
+        console.log(timer)
+        if (timer === 0) {
+        clearInterval(myTimer);
+        timer.innerHTML = "EXPIRED";}
+    }, 1000);
+    
     
 }
+
+//new function (question/answer)
