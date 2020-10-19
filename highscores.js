06e46e0f9c333eef89935c04bb657277c76c5ce2
Sarah <70615195+smanter82@users.noreply.github.com>
2020-10-19 17:48:33 -0400

    Pulled yourScore from local storage

--- highscores.js
+++ highscores.js
@@ -1,11 +1,14 @@
 let inputInitials = document.getElementById("inputInitials")
 let submitButton = document.querySelector(".submitButton")
 let initials = inputInitials.textContent
-
+let yourScore = document.querySelector(".yourScore")
+let score = localStorage.getItem("score")
 
 submitButton.addEventListener("click", function(event){
     event.preventDefault
     console.log(initials)
 })
 
+
+yourScore.textContent = "Your score = " + score
 
